d41c6b707f61e285be63611742219d327c532866
scty11 <scotty_wright@my.open.ac.uk>
2014-12-22 12:23:23 +0000

    added event example

---
+++ MyBuild/MyBuild/EventArgsEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBuild
+{
+    class EventArgsEx : System.EventArgs
+    {
+        public EventArgsEx(int hours, WorkType workType)
+        {
+            Hours = hours;
+            WorkType = workType;
+        }
+
+        public int Hours { get; set; }
+        public WorkType WorkType { get; set; }
+    }
+}
---
+++ MyBuild/MyBuild/EventEx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBuild
+{
+    //public delegate void WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
+    class EventEx
+    {
+        //public event WorkPerformedHandler WorkPerformed;
+
+        //These two commeneted out can be replaced by the EventHandler<t> if we dont
+        //wish to use the delagate directly. as shown above
+
+        public event EventHandler<EventArgsEx> WorkPerformed;
+        public event EventHandler WorkComplete;
+
+        public void DoWork(int hour, WorkType y) 
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //On.... is a naming convention for the event.
+                OnWorkPerformed(i + 1, y);
+            }
+            OnWorkComplete();
+        }
+
+        //The On methods are virtual so they can overidden
+        protected virtual void OnWorkComplete()
+        {
+            if (WorkComplete != null)
+            {
+                WorkComplete(this, EventArgs.Empty);
+            }
+        }
+
+        protected virtual void OnWorkPerformed(int p, WorkType y)
+        {
+            //two ways to invoke the delegate. 
+            //using the event it self.
+            if (WorkPerformed != null)
+            {
+                WorkPerformed(this, new EventArgsEx(p,y));
+            }
+            //or using the underlying delegate directly.
+
+            //var del = WorkPerformed as EventHandler<EventArgsEx>;
+            //if (del != null)
+            //{
+            //    del(this, new EventArgsEx(p, y));
+            //}
+        }
+    }
+}
