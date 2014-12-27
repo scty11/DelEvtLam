aab1b63c935c335faa3854e852e8cd0b332bd773
scty11 <scotty_wright@my.open.ac.uk>
2014-12-27 13:56:42 +0000

    added comm between components

---
+++ MyBuild/MyBuild/ProcessData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBuild
+{
+    class ProcessData
+    {
+        public static int ProcessSomeData(int x, int y, ProcessedDelegate del)
+        {
+            return del(x, y);
+        }
+
+        public static void OtherData(int x, int y, Action<int, int>action){
+            
+            action(x, y);
+        }
+    }
+}
--- MyBuild/MyBuild/Program.cs
+++ MyBuild/MyBuild/Program.cs
@@ -6,22 +6,36 @@ using System.Threading.Tasks;
 
 namespace MyBuild
 {
+    public delegate int ProcessedDelegate(int x, int y);
     class Program
     {
+        
         static void Main(string[] args)
         {
             //DelegateEx.Example();
 
             var eventEx = new EventEx();
             eventEx.WorkPerformed += eventEx_WorkPerformed;
-            eventEx.WorkComplete += eventEx_WorkComplete;
-            eventEx.DoWork(5, WorkType.PaperWork);
+            eventEx.WorkComplete += (x, y) => Console.WriteLine("The work has complete.");            
+            //eventEx.WorkComplete += eventEx_WorkComplete;
+            //eventEx.DoWork(5, WorkType.PaperWork);
+           
+            ProcessedDelegate AddDel = (x, y) => { return x + y; };
+            ProcessedDelegate DelDel = (x, y) => { return x + y; };
+            var result = ProcessData.ProcessSomeData(5, 1, AddDel);
+            Console.WriteLine(result);
+
+            //This removes the need to create a delegate like the one above Program.
+            Action<int,int> ADel = (x,y) => { Console.WriteLine(x + y); };
+            ProcessData.OtherData(5, 1, ADel);
+
             Console.Read();
         }
 
         static void eventEx_WorkComplete(object sender, EventArgs e)
         {
             Console.WriteLine("The work has complete.");
+            
         }
 
         static void eventEx_WorkPerformed(object sender, EventArgsEx e)
